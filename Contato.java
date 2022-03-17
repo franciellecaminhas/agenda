54050c976ea70d033072abe864f9e3bee21c2cc3
Francielle Caminhas <franciellegonsi@hotmail.com>
2022-03-17 11:17:03 -0300

    Primeira versão

---
+++ Contato.java
@@ -0,0 +1,110 @@
+package com.senac.agendatelefonica;
+
+public class Contato {
+    private int codigo;
+    private String nome;
+    private String endereço;
+    private String cidade;
+    private String estado;
+    private String telefone;
+    private String celular;
+    private String dataNasc;
+    private String email;
+    private String comentario;
+    
+    public void incluir(){
+        
+    }
+    public void salvar(){
+        
+    }
+    
+    public void excluir(){
+        
+    }
+    
+    public void sair(){
+        
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setEndereço(String endereço) {
+        this.endereço = endereço;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public void setDataNasc(String dataNasc) {
+        this.dataNasc = dataNasc;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setComeentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEndereço() {
+        return endereço;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public String getDataNasc() {
+        return dataNasc;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+    
+}
